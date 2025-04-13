a8e15811b64e3ac6da8a2c32ebc671f94421f721
nguyen-v <vincen7.nguyen@gmail.com>
2025-04-13 10:47:16 +0200

    change udev rules

--- scripts/record_dataset.sh
+++ scripts/record_dataset.sh
@@ -2,17 +2,18 @@
 
 
 TASK=${1:-"Grasp a lego block and put it in the bin."}
-REPO_ID=${2:-"${HF_USER}/so100_pi0"}
+REPO_ID=${2:-"${HF_USER}/so100_press_red_button_1ep"}
 
-python lerobot/scripts/control_robot.py \
+python scripts/control_robot.py \
   --robot.type=so100 \
   --control.type=record \
   --control.fps=30 \
   --control.single_task="$TASK" \
   --control.repo_id="$REPO_ID" \
   --control.tags='["so100","tutorial"]' \
-  --control.warmup_time_s=5 \
+  --control.warmup_time_s=15 \
   --control.episode_time_s=30 \
-  --control.reset_time_s=30 \
-  --control.num_episodes=2 \
-  --control.push_to_hub=true
+  --control.reset_time_s=10 \
+  --control.num_episodes=1 \
+  --control.push_to_hub=true \
+  --control.display_data=true
--- scripts/teleoperate.sh
+++ scripts/teleoperate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python lerobot/scripts/control_robot.py   --robot.type=so100   --robot.cameras='{}'   --control.type=teleoperate
+# python lerobot/scripts/control_robot.py   --robot.type=so100   --robot.cameras='{}'   --control.type=teleoperate
+
+python lerobot/scripts/control_robot.py   --robot.type=so100   --control.display_data=true --control.type=teleoperate 
